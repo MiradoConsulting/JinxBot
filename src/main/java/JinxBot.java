f5b8da58932563750346ea585d9921ca8ee7ce9d
davidvdc <85992394+davidvdc@users.noreply.github.com>
2024-04-05 18:08:18 +0200

    lets gooo

---
+++ src/main/java/JinxBot.java
@@ -0,0 +1,34 @@
+package src.main.java;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class JinxBot extends Robot {
+
+    public void run() {
+        while(true) {
+            scan();
+            turnLeft(5);
+            ahead(100);
+        }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        double bearing = event.getBearing();
+        double gunHeading = getGunHeading();
+
+        double diff = bearing - gunHeading;
+        turnGunLeft(diff);
+        fire(1);
+        turnGunRight(1);
+        fire(1);
+        turnGunRight(1);
+        fire(1);
+        turnGunRight(1);
+        fire(1);
+        turnGunRight(1);
+        fire(1);
+        turnGunRight(1);
+    }
+}
