747e7236b5d58730e51e4858090a3df330f3c289
davidvdc <85992394+davidvdc@users.noreply.github.com>
2024-04-05 19:35:55 +0200

    lets gooo

--- src/main/java/Jinx.java
+++ src/main/java/Jinx.java
@@ -8,23 +8,55 @@ import java.awt.*;
 
 public class Jinx extends Robot {
 
-    public void run() {
+    public void mode1() {
         setAllColors(Color.cyan);
         setGunColor(Color.MAGENTA);
         setRadarColor(Color.MAGENTA);
+        back(100);
+        turnGunLeft(180);
+        turnLeft(15);
+        ahead(300);
+        turnGunLeft(180);
+        turnLeft(15);
+
+    }
+
+    public void mode2() {
+        setAllColors(Color.BLUE);
+        setGunColor(Color.black);
+        setRadarColor(Color.black);
+        ahead(1);
+        turnLeft(1);
+        turnGunLeft(1);
+    }
+
+    int mode = 1;
+long round = 0;
+
+    public void run() {
+
         while(true) {
-            back(100);
-            turnGunLeft(180);
-            turnLeft(15);
-            ahead(300);
-            turnGunLeft(180);
-            turnLeft(15);
+            round ++;
+            if(mode == 1) {
+                mode1();
+            } else {
+                mode2();
+            }
+
+            if(round > 100) {
+                round = 100;
+                if(Math.random() > 0.5) {
+                    mode = 1;
+                } else {
+                    mode = 2;
+                }
+            }
         }
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-        if(getEnergy() > 1) {
+        if(getEnergy() > 1 && event.getDistance() < 300) {
             fire(1);
         }
     }
