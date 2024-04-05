d211349f1ba53584c01e1595a4d98885e1d6790b
davidvdc <85992394+davidvdc@users.noreply.github.com>
2024-04-05 18:54:47 +0200

    lets gooo

--- src/main/java/Jinx.java
+++ src/main/java/Jinx.java
@@ -3,9 +3,12 @@ import robocode.HitWallEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
+import java.awt.*;
+
 public class Jinx extends Robot {
 
     public void run() {
+        setAllColors(Color.cyan);
         while(true) {
             back(50);
             turnRadarLeft(360);
